20e6063ede3cb928fe3ad913e8e7241a46b35c1b
Morientis <twistedtime405@gmail.com>
2025-03-25 11:53:59 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const $list = document.querySelectorAll('li');
+
+function activeLink() {
+    $list.forEach(($li) => {
+        $li.classList.remove('active')
+    });
+    this.classList.add('active');
+}
+
+$list.forEach(($li) => {
+    $li.addEventListener(
+        'mouseover',
+        activeLink,
+    );
+});
+
